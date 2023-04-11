4be4c4d444f015d9c5925c976fb9e37c701eb657
Egbetimmy <egbetiimmy@gmail.com>
2023-04-11 01:49:48 +0100

    python file

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,14 +1,29 @@
-import datetime
 import mysql.connector
 
 __cnx = None
 
+
 def get_sql_connection():
-  print("Opening mysql connection")
-  global __cnx
+    print("Opening mysql connection")
+    global __cnx
 
-  if __cnx is None:
-    __cnx = mysql.connector.connect(user='root', password='root', database='grocery_store')
+    if __cnx is None:
+        __cnx = mysql.connector.connect(user='root', password='tanrose1996', database='grocery_app')
 
-  return __cnx
+    return __cnx
 
+
+'''
+from sqlalchemy import create_engine
+
+engine = None
+
+def get_sql_connection():
+    global engine
+    
+    if engine is None:
+        engine = create_engine('mysql+mysqlconnector://root:tanrose1996@localhost/grocery_app', echo=True)
+
+    return engine.connect()
+
+'''
