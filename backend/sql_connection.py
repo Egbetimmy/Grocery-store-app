f8fa1ac0ff44acd458ff1a6cf7bb8d525e684427
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2023-09-18 19:32:29 +0100

    python file

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,4 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load the .env file into environment variables
+load_dotenv()
+
+# Access environment variables
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
 __cnx = None
 
@@ -8,22 +18,11 @@ def get_sql_connection():
     global __cnx
 
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='Timmy', password='tanrose1996', database='grocery_app')
+        __cnx = mysql.connector.connect(
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
 
     return __cnx
 
-
-'''
-from sqlalchemy import create_engine
-
-engine = None
-
-def get_sql_connection():
-    global engine
-    
-    if engine is None:
-        engine = create_engine('mysql+mysqlconnector://root:tanrose1996@localhost/grocery_app', echo=True)
-
-    return engine.connect()
-
-'''
